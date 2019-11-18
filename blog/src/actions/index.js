f534dadaf8d628198c5054c685376d9f759ace62
Jeena Arjun <jeenarjun@JeenaChathysMBP.localdomain>
2019-11-18 14:52:49 -0800

    FInd unique user ids

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,13 +1,6 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log("going to fetch");
-  await dispatch(fetchPosts());
-  console.log("fetched posts");
-  // await dispatch(fetchUser());
-};
-
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
@@ -29,3 +22,10 @@ export const fetchUser = id => async dispatch => {
 //   const response = await jsonPlaceholder.get(`/users/${id}`);
 //   dispatch({ type: "FETCH_USER", payload: response.data });
 // });
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
