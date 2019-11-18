41c330de5f1c95873e116a885004a1fa7db43247
Jeena Arjun <jeenarjun@Jeena-Chemparathys-MacBook-Pro.local>
2019-11-17 21:38:51 -0800

    Create songs folder

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,35 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:05"
+    },
+    {
+      title: "All Star",
+      duration: "3:30"
+    },
+    {
+      title: "Barbie Girl",
+      duration: "3:45"
+    },
+    {
+      title: "Wakka Wakka",
+      duration: "2:05"
+    }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
